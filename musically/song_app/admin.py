d4d76e85810de4324d985993a853940039eeb774
Калоян Милчев <106106437+Kaloyan9905@users.noreply.github.com>
2023-08-08 17:20:18 +0300

    Add files via upload

--- musically/song_app/admin.py
+++ musically/song_app/admin.py
@@ -6,7 +6,15 @@ from musically.song_app.models import SongFile, PersonalSongs
 # Register your models here.
 @admin.register(SongFile)
 class SongFileAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['title', 'artist', 'uploaded_at']
+    search_fields = ['title', 'artist']
+    list_filter = ['artist', 'uploaded_at']
+    ordering = ['-uploaded_at']
+    fields = ['title', 'artist', 'song_image_url', 'audio_file']
+    list_per_page = 10
+    date_hierarchy = 'uploaded_at'
+    admin.site.site_title = 'My Music App Admin'
+    admin.site.site_header = 'My Music App Administration'
 
 
 @admin.register(PersonalSongs)
